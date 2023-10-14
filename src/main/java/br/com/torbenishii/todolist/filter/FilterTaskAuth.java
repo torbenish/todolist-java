d9ab7fc15c1b7707539e65f3029794813b2935fb
Torben Ishii <ishiitorben@gmail.com>
2023-10-14 12:06:44 -0300

    Updating tasks and validating scales

--- src/main/java/br/com/torbenishii/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/torbenishii/todolist/filter/FilterTaskAuth.java
@@ -14,46 +14,57 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
- @Component
-public class FilterTaskAuth extends OncePerRequestFilter{
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
 
-  @Autowired
-  private IUserRepository userRepository;
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var servletPath = request.getServletPath();
+
+        if(servletPath.startsWith("/tasks/")) {
+
+            //Pegar a autenticacao(usuario e senha)
+            var authorization = request.getHeader("Authorization");
 
 
+            var authEncoded = authorization.substring("Basic".length()).trim();
 
-  @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-      throws ServletException, IOException {
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
 
-        // Pegar a autentication (user and password)
-        var authorization = request.getHeader("Authorization");
-        var authEncoded = authorization.substring("Basic".length()).trim();
+            var authString = new String(authDecode);
 
-        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
 
-        var authString = new String(authDecode);
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials [1];
 
-        String[] credentials = authString.split(":");
-        String username = credentials[0];
-        String password = credentials[1];
+            //validar usuario
+            var user = this.userRepository.findByUsername(username);
+            if(user == null) {
+                response.sendError(401);
+            }else {
+                //validar senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if(passwordVerify.verified) {
+                    //segue viagem
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
 
-        // Validar user
-        var user = this.userRepository.findByUsername(username);
-        if(user == null) {
-          response.sendError(401);
+
+            }
         } else {
           filterChain.doFilter(request, response);
-        }
-        // Validar password
+        }       
 
-        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-        if(passwordVerify.verified) {
-          filterChain.doFilter(request, response);
-        } else {
-          response.sendError(401);
-        }
-        // Segue viagem
-  }
+    }
 
+   
 }
--- src/main/java/br/com/torbenishii/todolist/task/TaskController.java
+++ src/main/java/br/com/torbenishii/todolist/task/TaskController.java
@@ -1,10 +1,22 @@
 package br.com.torbenishii.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.torbenishii.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,12 +26,54 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel) {
-    System.out.println("Chegou no controller");
+  public ResponseEntity create(@RequestBody TaskModel taskModel,HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    taskModel.setIdUser((UUID) idUser);
+
+    var currentDate = LocalDateTime.now();
+
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("A data de inicio / data de termino deve ser maior do que a data atual");
+    }
+
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("A data de inicio deve ser menor do que a data de termino ");
+    }
+
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
   }
 
+  @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+  
+   @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
 
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        if(task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body("Tarefa nao encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if(!task.getIdUser().equals(idUser)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body("usuario nao tem permissao para alterar essa tarefa");
+        }
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
+    }
 
 }
